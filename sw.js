d08ee0de493573b0850c89db05b2e869d42a214b
kadek89 <168944489+kadek89@users.noreply.github.com>
2026-09-24 21:01:42 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -12,6 +12,15 @@ self.addEventListener("install", (e) => {
   self.skipWaiting();
 });
 
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
 self.addEventListener("fetch", (e) => {
   e.respondWith(
     caches.match(e.request).then((res) => res || fetch(e.request))
