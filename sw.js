2f0ef36fd3e9a4ea90e7116648eb8de9b55ba6ab
kadek89 <168944489+kadek89@users.noreply.github.com>
2026-09-25 17:54:35 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,11 @@
-const CACHE_NAME = "getcar-v4";
+const CACHE_NAME = "getcar-v6";
 
 const ASSETS = [
   "index.html",
   "login.html",
   "pendaftaran.html",
   "gc.png",
+  "gc-192.png",
   "get.png",
   "logoGC.png",
   "awal.png",
@@ -23,9 +24,7 @@ self.addEventListener("install", (e) => {
 self.addEventListener("activate", (e) => {
   e.waitUntil(
     caches.keys().then((keys) =>
-      Promise.all(
-        keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k))
-      )
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
     )
   );
   self.clients.claim();
@@ -33,11 +32,7 @@ self.addEventListener("activate", (e) => {
 
 self.addEventListener("fetch", (e) => {
   if (e.request.method !== "GET") return;
-
   const url = new URL(e.request.url);
   if (url.origin !== location.origin) return;
-
-  e.respondWith(
-    caches.match(e.request).then((res) => res || fetch(e.request))
-  );
+  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
 });
